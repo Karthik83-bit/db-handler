c90a24da248be4df57700448dc217980dc672e88
Karthik83-bit <70068596+Karthik83-bit@users.noreply.github.com>
2026-04-11 08:25:50 +0530

    Update db-config.js

--- db-config.js
+++ db-config.js
@@ -2,46 +2,46 @@
 const dbConfigsByEnvironment = {
   uat: {
     postgres: {
-      label: "PostgreSQL",
-      host: "34.93.233.58",
-      port: "5432",
-      user: "kotak_uat_rw",
-      password: "TYnb2#819",
-      database: "kotak_uat"
+      label:process.env.POSTGRES_LABEL,
+      host: process.env.POSTGRES_HOST,
+      port: process.env.POSTGRES_PORT,
+      user: process.env.POSTGRES_USER ,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE ,
     },
     cassandra: {
-      label: "Cassandra",
-      contactPoints: ["34.93.148.99"],
-      localDataCenter: "datacenter1",
-      keyspace: "kotak_uat",
-      username: "kotak_uat_rw",
-      password: "K0+@k&U@t5O!50$"
+      label: process.env.CASSANDRA_LABEL,
+      contactPoints: process.env.CASSANDRA_CONTACT_POINTS ? process.env.CASSANDRA_CONTACT_POINTS.split(",") : ["34.93.148.99"],
+      localDataCenter: process.env.CASSANDRA_LOCAL_DATA_CENTER,
+      keyspace: process.env.CASSANDRA_KEYSPACE,
+      username: process.env.CASSANDRA_USERNAME,
+      password: process.env.CASSANDRA_PASSWORD,
     },
     mongodb: {
-      label: "MongoDB",
-      uri: "mongodb://mongorwuser:rw%404456@35.200.190.68:27017/mongostage?directConnection=true&tls=false",
-      database: "mongostage"
+      label: process.env.MONGODB_LABEL,
+      uri: process.env.MONGODB_URI,
+      database: process.env.MONGODB_DATABASE
     }
   },
   stage: {
     postgres: {
-      label: "PostgreSQL",
-      host: "127.0.0.1",
-      port: 5432,
-      user: "stage_postgres",
-      password: "stage_postgres123",
-      database: "stage_sampledb"
+      label: process.env.POSTGRES_LABEL || "PostgreSQL",
+      host: process.env.POSTGRES_HOST || "127.0.0.1",
+      port: process.env.POSTGRES_PORT || 5432,
+      user: process.env.POSTGRES_USER || "stage_postgres",
+      password: process.env.POSTGRES_PASSWORD || "stage_postgres123",
+      database: process.env.POSTGRES_DATABASE || "stage_sampledb"
     },
     cassandra: {
-      label: "Cassandra",
-      contactPoints: ["127.0.0.1"],
-      localDataCenter: "datacenter1",
-      keyspace: "system"
+      label: process.env.CASSANDRA_LABEL || "Cassandra",
+      contactPoints: process.env.CASSANDRA_CONTACT_POINTS ? process.env.CASSANDRA_CONTACT_POINTS.split(",") : ["127.0.0.1"],
+      localDataCenter: process.env.CASSANDRA_LOCAL_DATA_CENTER || "datacenter1",
+      keyspace: process.env.CASSANDRA_KEYSPACE ||  "system"
     },
     mongodb: {
-      label: "MongoDB",
-      uri: "mongodb://127.0.0.1:27017",
-      database: "stage_admin"
+      label: process.env.MONGODB_LABEL || "MongoDB",
+      uri: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017",
+      database: process.env.MONGODB_DATABASE || "stage_admin"
     }
   },
   prod: {
